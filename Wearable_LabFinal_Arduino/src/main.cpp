992eef13bee9b51974aeb2229daa633b4e379c5c
Davide Giacomini <giacomini.davide@outlook.com>
2021-12-02 11:27:12 -0600

    Syntax typo

--- Wearable_LabFinal_Arduino/src/main.cpp
+++ Wearable_LabFinal_Arduino/src/main.cpp
@@ -187,6 +187,8 @@
 
 
 //////////////////////////////
+// LAB1 OXYMETER
+
 // #include <SparkFun_Bio_Sensor_Hub_Library.h>
 // #include <Wire.h>
 
@@ -320,58 +322,7 @@
 // }
 
 
-
-// TEMPLATE THERMOMETER
-// #include <Wire.h>
-// // #include <TinyWireM.h>
-// #include <Adafruit_MiniMLX90614.h>
-// // #include <Adafruit_NeoPixel.h>
-
-// // change these to adjust the range of temperatures you want to measure 
-// // (these are in Farenheit)
-// #define COLDTEMP 60
-// #define HOTTEMP  80
-
-
-// #define PIN 1
-// // Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
-
-// Adafruit_MiniMLX90614 mlx = Adafruit_MiniMLX90614();
-
-// void setup() {
-//   mlx.begin();  
-//   // strip.begin();
-//   // strip.show(); // Initialize all pixels to 'off'
-// }
-
-// void loop() {
-//   uint8_t red, blue;
-//   float temp = mlx.readObjectTempF();
-
-//   Serial.println(temp);
-
-//   // if (temp < COLDTEMP) temp = COLDTEMP;
-//   // if (temp > HOTTEMP) temp = HOTTEMP;
-
-//   // // map temperature to red/blue color
-//   // // hotter temp -> more red
-//   // red = map(temp, COLDTEMP, HOTTEMP, 0, 255);  
-//   // // hotter temp -> less blue
-//   // blue = map(temp, COLDTEMP, HOTTEMP, 255, 0);  
-
-//   // colorWipe(strip.Color(red, 0, blue), 0);
-  
-//   delay(500); // can adjust this for faster/slower updates
-// }
-
-// // Fill the dots one after the other with a color
-// // void colorWipe(uint32_t c, uint8_t wait) {
-// //   for(uint16_t i=0; i<strip.numPixels(); i++) {
-// //       strip.setPixelColor(i, c);
-// //       strip.show();
-// //       delay(wait);
-// //   }
-// // }
+// EXAMPLE THERMOMETER
 
 #include <Wire.h> // I2C library, required for MLX90614
 #include <SparkFunMLX90614.h> //Click here to get the library: http://librarymanager/All#Qwiic_IR_Thermometer by SparkFun
